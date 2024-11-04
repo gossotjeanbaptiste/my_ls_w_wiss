a6f6ab96ce94aa35d9fc1405ac790afe44877140
Ethan Wiss <ethan.wiss@epitech.eu>
2024-11-04 18:44:32 +0100

    adding my_ls

--- my_ls.c
+++ my_ls.c
@@ -2,28 +2,110 @@
 ** EPITECH PROJECT, 2024
 ** collab
 ** File description:
-** my_ls
+** test
 */
-
 #include "my.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <unistd.h>
+#include <sys/sysmacros.h>
 #include <pwd.h>
+#include <grp.h>
 #include <time.h>
-#include "test.h"
+#include <string.h>
 
-int stat(const char *path, struct stat *buf);
+char *my_strrchr(const char *str, int c)
+{
+    const char *last_occurrence = NULL;
 
-// int main(struct stat stats)
-// {
-//     struct tm dt;
+    while (*str) {
+        if (*str == (char)c) {
+            last_occurrence = str;
+        }
+        str++;
+    }
+    return (char *)last_occurrence;
+}
 
-//     dt = *(gmtime(&stats.st_ctime));
-//     printf("\nCreated on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900, dt.tm_hour, dt.tm_min, dt.tm_sec);
-//     return 0;
-// }
+static void display_file_type(mode_t mode)
+{
+    if (S_ISREG(mode))
+        my_printf("f\n");
+    if (S_ISDIR(mode))
+        my_printf("d\n");
+    if (S_ISCHR(mode))
+        my_printf("c\n");
+    if (S_ISBLK(mode))
+        my_printf("b\n");
+    if (S_ISFIFO(mode))
+        my_printf("p\n");
+    if (S_ISLNK(mode))
+        my_printf("l\n");
+    if (S_ISSOCK(mode))
+        my_printf("s\n");
+}
+
+static void display_device_info(mode_t mode, dev_t rdev)
+{
+    if (S_ISCHR(mode) || S_ISBLK(mode)) {
+        my_printf("Minor: %d\n", minor(rdev));
+        my_printf("Major: %d\n", major(rdev));
+    } else {
+        my_printf("Minor: N/A\n");
+        my_printf("Major: N/A\n");
+    }
+}
+
+static void display_all(const char *base_name, struct stat *stats)
+{
+    my_printf("Name: %s\n", base_name);
+    my_printf("Type: ");
+    display_file_type(stats->st_mode);
+    my_printf("Inode: %d\n", stats->st_ino);
+    my_printf("Hard Link: %d\n", stats->st_nlink);
+    my_printf("Size: %d\n", stats->st_size);
+    my_printf("Allocated space: %d\n", stats->st_blocks * 512);
+    display_device_info(stats->st_mode, stats->st_rdev);
+    my_printf("UID: %d\n", stats->st_uid);
+    my_printf("GID: %d\n", stats->st_gid);
+}
+
+void display_properties(const char *filename, struct stat *stats)
+{
+    char temp[1024];
+    size_t len;
+    const char *base_name;
+
+    my_strncpy(temp, filename, sizeof(temp) - 1);
+    temp[sizeof(temp) - 1] = '\0';
+    len = my_strlen(temp);
+    if (len > 1 && temp[len - 1] == '/') {
+        temp[len - 1] = '\0';
+    }
+    base_name = my_strrchr(temp, '/');
+    if (base_name != NULL) {
+        base_name++;
+    } else {
+        base_name = temp;
+    }
+    display_all(base_name, stats);
+}
+
+int main(int argc, char *argv[])
+{
+    struct stat stats;
+
+    if (argc != 2) {
+        my_printf("Usage: %s <file_path>\n", argv[0]);
+        return 1;
+    }
+    if (stat(argv[1], &stats) == 0) {
+        display_properties(argv[1], &stats);
+    } else {
+        my_printf("File not found\n");
+        return 84;
+    }
+    return 0;
+}
--- test.c
+++ test.c
@@ -4,25 +4,94 @@
 ** File description:
 ** test
 */
-
-/**
- * C program to find file permission, size, creation and last modification date of 
- * a given file.
- */
-
 #include "my.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <unistd.h>
+#include <sys/sysmacros.h>
 #include <pwd.h>
+#include <grp.h>
 #include <time.h>
-#include "test.h"
+#include <string.h>
 
-void printFileProperties(struct stat stats);
+char *my_strrchr(const char *str, int c)
+{
+    const char *last_occurrence = NULL;
+
+    while (*str) {
+        if (*str == (char)c) {
+            last_occurrence = str;
+        }
+        str++;
+    }
+    return (char *)last_occurrence;
+}
+
+static void display_file_type(mode_t mode)
+{
+    if (S_ISREG(mode))
+        my_printf("f\n");
+    if (S_ISDIR(mode))
+        my_printf("d\n");
+    if (S_ISCHR(mode))
+        my_printf("c\n");
+    if (S_ISBLK(mode))
+        my_printf("b\n");
+    if (S_ISFIFO(mode))
+        my_printf("p\n");
+    if (S_ISLNK(mode))
+        my_printf("l\n");
+    if (S_ISSOCK(mode))
+        my_printf("s\n");
+}
+
+static void display_device_info(mode_t mode, dev_t rdev)
+{
+    if (S_ISCHR(mode) || S_ISBLK(mode)) {
+        my_printf("Minor: %d\n", minor(rdev));
+        my_printf("Major: %d\n", major(rdev));
+    } else {
+        my_printf("Minor: N/A\n");
+        my_printf("Major: N/A\n");
+    }
+}
+
+static void display_all(const char *base_name, struct stat *stats)
+{
+    my_printf("Name: %s\n", base_name);
+    my_printf("Type: ");
+    display_file_type(stats->st_mode);
+    my_printf("Inode: %d\n", stats->st_ino);
+    my_printf("Hard Link: %d\n", stats->st_nlink);
+    my_printf("Size: %d\n", stats->st_size);
+    my_printf("Allocated space: %d\n", stats->st_blocks * 512);
+    display_device_info(stats->st_mode, stats->st_rdev);
+    my_printf("UID: %d\n", stats->st_uid);
+    my_printf("GID: %d\n", stats->st_gid);
+}
+
+void display_properties(const char *filename, struct stat *stats)
+{
+    char temp[1024];
+    size_t len;
+    const char *base_name;
+
+    my_strncpy(temp, filename, sizeof(temp) - 1);
+    temp[sizeof(temp) - 1] = '\0';
+    len = my_strlen(temp);
+    if (len > 1 && temp[len - 1] == '/') {
+        temp[len - 1] = '\0';
+    }
+    base_name = my_strrchr(temp, '/');
+    if (base_name != NULL) {
+        base_name++;
+    } else {
+        base_name = temp;
+    }
+    display_all(base_name, stats);
+}
 
 int main(int argc, char *argv[])
 {
@@ -32,46 +101,11 @@ int main(int argc, char *argv[])
         my_printf("Usage: %s <file_path>\n", argv[0]);
         return 1;
     }
-
-    // stat() returns 0 on successful operation,
-    // otherwise returns -1 if unable to get file properties.
     if (stat(argv[1], &stats) == 0) {
-        printFileProperties(stats);
+        display_properties(argv[1], &stats);
     } else {
-        my_printf("Unable to get file properties.\n");
-        my_printf("Please check whether '%s' file exists.\n", argv[1]);
+        my_printf("File not found\n");
+        return 84;
     }
-
     return 0;
-}
-
-/**
- * Function to print file properties.
- */
-void printFileProperties(struct stat stats)
-{
-    struct tm dt;
-
-    // File permissions
-    my_printf("\nFile access: ");
-    if (stats.st_mode & R_OK)
-        my_printf("read ");
-    if (stats.st_mode & W_OK)
-        my_printf("write ");
-    if (stats.st_mode & X_OK)
-        my_printf("execute");
-
-    // File size
-    my_printf("\nFile size: %d", stats.st_size);
-
-    // Get file creation time in seconds and 
-    // convert seconds to date and time format
-    dt = *(gmtime(&stats.st_ctime));
-    my_printf("\nCreated on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon + 1, dt.tm_year + 1900, 
-                                              dt.tm_hour, dt.tm_min, dt.tm_sec);
-
-    // File modification time
-    dt = *(gmtime(&stats.st_mtime));
-    my_printf("\nModified on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon + 1, dt.tm_year + 1900, 
-                                              dt.tm_hour, dt.tm_min, dt.tm_sec);
 }
