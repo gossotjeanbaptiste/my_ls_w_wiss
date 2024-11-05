b4edcfc8d48db244390763d3f9cab809f578fad5
Jean-Baptiste GOSSOT <jean-baptiste.gossot@epitech.eu>
2024-11-05 12:26:37 +0100

    coding style

--- lib/my/my_putstr.c
+++ lib/my/my_putstr.c
@@ -5,25 +5,12 @@
 ** display each char of a string
 */
 #include "my.h"
+#include <unistd.h>
 
 int my_putstr(char const *str)
 {
-    int i = 0;
+    int len = my_strlen(str);
 
-    while (str[i] != '\0'){
-        my_putchar(str[i]);
-        i++;
-    }
-    return 0;
-}
-
-int my_putnstr(char const *str, int precision)
-{
-    int i = 0;
-
-    while (str[i] != '\0' && i < precision){
-        my_putchar(str[i]);
-        i++;
-    }
+    write(1, str, len);
     return 0;
 }
